454fda1275c2857c17876ff95e7cabec1d229309
Naman Malhotra <namanmalhotra451@gmail.com>
2026-08-15 19:46:02 +0530

    Update voxel.rb

--- voxel.rb
+++ voxel.rb
@@ -1,7 +1,7 @@
 class Voxel < Formula
   desc "Decentralized P2P Version Control System"
-  homepage "https://github.com/Naman1177/Voxel"
-  url "https://github.com/Naman1177/Voxel/archive/refs/tags/v1.0.0.tar.gz"
+  homepage "https://github.com/Naman1177/voxel"
+  url "https://github.com/Naman1177/voxel/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "4f6058e9b8febc77039de5c2033ba958197ea063444cc77f613d6ac664f96fb1"
   license "MIT"
 
