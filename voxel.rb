290242480fa5589b7c2b09f428a20c58c807ae2b
Naman Malhotra <namanmalhotra451@gmail.com>
2026-08-15 19:53:26 +0530

    Update voxel.rb

--- voxel.rb
+++ voxel.rb
@@ -5,13 +5,25 @@ class Voxel < Formula
   sha256 "4f6058e9b8febc77039de5c2033ba958197ea063444cc77f613d6ac664f96fb1"
   license "MIT"
 
-  # Explicitly tell Homebrew to bring these libraries into the build sandbox
   depends_on "mbedtls"
   depends_on "zstd"
 
   def install
-    # Added -lmbedcrypto and -lzstd so the linker can build the final executable
-    system "clang++", "-std=c++17", "-I", "inc", "-I", "third_party_lib", *Dir["src/*.cpp"], "-o", "voxel", "-lmbedcrypto", "-lzstd"
+    # Dynamically grab the paths for the user's specific Mac architecture
+    mbedtls = Formula["mbedtls"]
+    zstd = Formula["zstd"]
+
+    # Inject the exact Include (-I) and Library (-L) paths into the compiler
+    system "clang++", "-std=c++17", 
+           "-I", "inc", 
+           "-I", "third_party_lib", 
+           "-I#{mbedtls.opt_include}", 
+           "-I#{zstd.opt_include}",
+           *Dir["src/*.cpp"], 
+           "-o", "voxel", 
+           "-L#{mbedtls.opt_lib}", 
+           "-L#{zstd.opt_lib}", 
+           "-lmbedcrypto", "-lzstd"
     
     bin.install "voxel"
   end
