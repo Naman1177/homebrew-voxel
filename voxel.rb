02565368ae2888d4ff211cad1a1265484f73124d
Naman Malhotra <namanmalhotra451@gmail.com>
2026-08-15 19:49:33 +0530

    Update voxel.rb

--- voxel.rb
+++ voxel.rb
@@ -5,20 +5,18 @@ class Voxel < Formula
   sha256 "4f6058e9b8febc77039de5c2033ba958197ea063444cc77f613d6ac664f96fb1"
   license "MIT"
 
-  # If you are using standard macOS frameworks, you don't need many dependencies.
-  # But if you rely on an external Zstd library, uncomment the line below:
-  # depends_on "zstd"
+  # Explicitly tell Homebrew to bring these libraries into the build sandbox
+  depends_on "mbedtls"
+  depends_on "zstd"
 
   def install
-    # This executes your clang++ build command exactly as you run it in the terminal
-    system "clang++", "-std=c++17", "-I", "inc", "-I", "third_party_lib", *Dir["src/*.cpp"], "-o", "voxel"
+    # Added -lmbedcrypto and -lzstd so the linker can build the final executable
+    system "clang++", "-std=c++17", "-I", "inc", "-I", "third_party_lib", *Dir["src/*.cpp"], "-o", "voxel", "-lmbedcrypto", "-lzstd"
     
-    # This moves the compiled binary into the global Homebrew path so the user can type 'voxel' anywhere
     bin.install "voxel"
   end
 
   test do
-    # A quick test to ensure the binary installed correctly
     system "#{bin}/voxel", "--help"
   end
 end
